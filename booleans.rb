5a5643a062ef146a477065b27f16bdb40f27d67f
Andre Luis de Oliveira Josefino <ALJosefino@GMail.com>
2022-04-14 23:59:07 -0300

    booleans and conditionals

---
+++ booleans.rb
@@ -0,0 +1,25 @@
+puts ("AND = &&")
+puts ("true AND true")
+puts true && true
+puts ("false AND false")
+puts false && false
+puts ("true AND false")
+puts true && false
+puts ("false AND true")
+puts false && true
+
+puts ("OR = || ")
+puts ("true OR true")
+puts true || true
+puts ("false OR false")
+puts false || false
+puts ("true OR false")
+puts true || false
+puts ("false OR true")
+puts false || true
+
+puts ("NOT = !")
+puts ("!true")
+puts !true
+puts ("!false")
+puts !false
---
+++ conditionals.rb
@@ -0,0 +1,8 @@
+puts ("x equals 10, ruby syntax ")
+x = 10
+puts ("if x equal 10, show a confirmation message")
+if x == 10
+    puts ("Yes, it's true! X equal 10")
+else
+    puts ("No, it's false! X different 10")
+end
