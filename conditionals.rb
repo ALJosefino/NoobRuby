1ae83cead9358247d0bcd3b93a0998e01f4612a2
Andre Luis de Oliveira Josefino <ALJosefino@GMail.com>
2022-04-15 00:18:08 -0300

    conditionsals, testing else

--- conditionals.rb
+++ conditionals.rb
@@ -1,8 +1,17 @@
-puts ("x equals 10, ruby syntax ")
+puts ("x equals 10, ruby syntax x = 10, here x receive this number")
 x = 10
-puts ("if x equal 10, show a confirmation message")
+puts ("if x equal 10, x == (double =, it's ruby standard values comparison) show a confirmation message")
 if x == 10
-    puts ("Yes, it's true! X equal 10")
+    puts ("Yes, it's true! X is equal 10")
 else
-    puts ("No, it's false! X different 10")
+    puts ("No, it's false! X is different 10")
+end
+
+puts ("y equals 11, so y it's different from 10 like example before")
+y = 11
+puts ("else y different from 10, if is a first condition, next comparison will be with 'else'")
+if y == 10
+   puts ("Yes, it's true! y is equal 10")
+else
+   puts ("No, it's false! y is differente 10")
 end
