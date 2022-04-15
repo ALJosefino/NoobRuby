0ea890d2c51d2952d6bf7ac2e3c360fcbfae3f91
Andre Luis de Oliveira Josefino <ALJosefino@GMail.com>
2022-04-15 19:25:58 -0300

    equal or different conditionals with numbers and interactions

--- equal_or_different.rb
+++ equal_or_different.rb
@@ -1,3 +1,4 @@
+#My exercise, conditionals with numbers and interactions.
 print ("Equal or different \n")
 
 print ("Type a number: \n")
@@ -5,6 +6,7 @@ number1 = gets.chomp.to_f
 
 print ("Type another number \n")
 number2 = gets.chomp.to_f
+
 
 print ("Now, you put two numbers. \n")
 print ("First number typed: #{number1}. \n")
