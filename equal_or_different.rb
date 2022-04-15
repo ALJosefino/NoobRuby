85361049286404983f5ba5d32cfff00fd626a2ac
Andre Luis de Oliveira Josefino <ALJosefino@GMail.com>
2022-04-15 19:24:25 -0300

    equal or different conditionals with numbers and interactions

---
+++ equal_or_different.rb
@@ -0,0 +1,19 @@
+print ("Equal or different \n")
+
+print ("Type a number: \n")
+number1 = gets.chomp.to_f
+
+print ("Type another number \n")
+number2 = gets.chomp.to_f
+
+print ("Now, you put two numbers. \n")
+print ("First number typed: #{number1}. \n")
+print ("Second number typed: #{number2}. \n") 
+
+if number1 == number2
+    print ("Equal numbers! \n")
+else
+    print ("Different numbers! \n")
+end
+
+print ("Thank you and bye! \n")
