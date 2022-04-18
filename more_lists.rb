903e41d56418e368e75d088cdc4b886e409b57ff
André L Josefino <ALJosefino@gmail.com>
2022-04-17 22:13:04 -0300

    more list with push, <<

---
+++ more_lists.rb
@@ -0,0 +1,11 @@
+p ("One more list was created, but empity")
+more_list = []
+#next it's a way to add items on the list 
+more_list.push("Initiative", "Persistence")
+p ("After added items, with name_of_list.push and about entrepreneurship behaviour")
+p more_list
+p ("Indicated a position number one of list")
+p more_list[1]
+more_list << "Firmness"
+p ("Other way to input, use to name_of_list << 'Firmness' ")
+p more_list
