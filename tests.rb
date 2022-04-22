9d1be6801a33e22e2f51bf22764ef7aa25b14943
Andre Luis de Oliveira Josefino <ALJosefino@GMail.com>
2022-04-21 23:20:29 -0300

    These two files, specifically tests were coded blocks that I tested separated from tickets, tickets was defined weekdays by automated format but can be manipulated by users

--- tests.rb
+++ tests.rb
@@ -1,20 +1,9 @@
-today = Time.new
+puts ("Please type Yes or No")
+typed = gets.chomp
 
-weekday = today.wday
-
-case weekday
-when 0
-    puts ("Today is sunday")
-when 1
-    puts ("Today is monday")
-when 2
-    puts ("Today is tuesday")
-when 3
-    puts ("Today is wednesday")
-when 4
-    puts ("Today is thursday")
-when 5
-    puts ("Today is friday")
-when 6
-    puts ("Today is saturday")
+if typed == "Yes"
+    puts ("Yes was typed")  
+elsif typed == "No"
+    puts ("No was typed")   
 end
+   
--- tickets.rb
+++ tickets.rb
@@ -31,14 +31,6 @@
 
 puts ("Hello! Here you may know how much customer will pay for a ticket")
 
-puts ("Please! What's customer name?")
-customer_name = gets.chomp
-
-puts ("How old customer is #{customer_name}?")
-customer_age = gets.chomp.to_i
-
-puts ("Customer #{customer_name} is #{customer_age} years old")
-
 today = Time.new
 
 weekday = today.wday
@@ -68,4 +60,62 @@ when 6
 end
 
 puts ("Today is  #{today}")
-puts ("Do you want manipulating this weekday?")
+
+puts ("Do you want manipulating #{today} to another weekday? Yes or No") 
+change_today = gets.chomp
+
+while (change_today != "Yes") and (change_today != "No") do
+    puts ("Do you want manipulating #{today} to another weekday?") 
+    puts ("Please type Yes or No") 
+    change_today = gets.chomp
+end
+
+if change_today == "Yes"
+    puts ("Yes was typed, so it's allowed change weekday now.")
+    puts ("According ti this:")
+    puts ("Type number: O = today is sunday")
+    puts ("Type number: 1 = today is monday")
+    puts ("Type number: 2 = today is tuesday")
+    puts ("Type number: 3 = today is wednesday")
+    puts ("Type number: 4 = today is thursday")
+    puts ("Type number: 5 = today is friday")
+    puts ("Type number: 6 = today is saturday")
+    today = gets.chomp.to_i
+    
+    case today
+    when 0
+            today = "Sunday" 
+            puts ("So, now you choose that today is sunday")
+        when 1
+            today = "Monday"
+            puts ("So, now you choose that today is monday")
+        when 2
+            today = "Tuesday"
+            puts ("So, now you choose that today is tuesday")
+        when 3
+            today = "Wednesday"
+            puts ("So, now you choose that today is wednesday")
+        when 4
+            today = "Thursday"
+            puts ("So, now you choose that today is thursday")
+        when 5
+            today = "Friday"
+            puts ("So, now you choose that today is friday")
+        when 6
+            today = "Saturday"
+            puts ("So, now you choose that today is saturday")
+        end
+    
+
+elsif change_today == "No"
+    puts ("Weekday keeps on #{today}")
+end
+
+puts ("Please! What's customer name?")
+customer_name = gets.chomp
+
+puts ("How old customer is #{customer_name}?")
+customer_age = gets.chomp.to_i
+#system "clear"
+
+puts ("Customer #{customer_name} is #{customer_age} years old and today is #{today}")
