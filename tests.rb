cf9db12a239e9b7667154d67f6d38a26f36a32c7
Andre Luis de Oliveira Josefino <ALJosefino@GMail.com>
2022-04-22 21:31:35 -0300

    Calculate from birthdate how old customer is

--- tests.rb
+++ tests.rb
@@ -1,9 +1,22 @@
-puts ("Please type Yes or No")
-typed = gets.chomp
+puts ("What is customer birth year? Example: 1999")
+customer_birth_year = gets.chomp.to_i
 
-if typed == "Yes"
-    puts ("Yes was typed")  
-elsif typed == "No"
-    puts ("No was typed")   
-end
-   
+puts ("What is customer birth month? Example: 04 = April")
+customer_birth_month = gets.chomp.to_i
+
+puts ("What is customer birth day? Example: 10")
+customer_birth_day = gets.chomp.to_i
+
+puts ("Is this customer birthdate #{customer_birth_day}/#{customer_birth_month}/#{customer_birth_year}?")
+
+ require 'date'
+
+ def customer_years_old(day, month, year)   
+  birthdate = Time.new(year, month, day)
+  average_seconds_in_four_years = 31557600
+  seconds = (Time.now - birthdate).to_i
+  years_old = seconds / average_seconds_in_four_years
+  years_old
+ end
+
+ puts customer_years_old(customer_birth_day, customer_birth_month, customer_birth_year)
--- tickets.rb
+++ tickets.rb
@@ -149,11 +149,25 @@ elsif change_today == "No"
 puts ("Weekday keeps on #{today}")
 end
 
-puts ("Please! What's customer name?")
-customer_name = gets.chomp
+puts ("What is customer birth year? Example: 1999")
+customer_birth_year = gets.chomp.to_i
 
-puts ("How old customer is #{customer_name}?")
-customer_age = gets.chomp.to_i
-#system "clear"
+puts ("What is customer birth month? Example: 04 = April")
+customer_birth_month = gets.chomp.to_i
 
-puts ("Customer #{customer_name} is #{customer_age} years old and today is #{today}")
+puts ("What is customer birth day? Example: 10")
+customer_birth_day = gets.chomp.to_i
+
+puts ("Is this customer birthdate #{customer_birth_day}/#{customer_birth_month}/#{customer_birth_year}?")
+
+require 'date'
+
+def customer_years_old(day, month, year)   
+ birthdate = Time.new(year, month, day)
+ average_seconds_in_four_years = 31557600
+ seconds = (Time.now - birthdate).to_i
+ years_old = seconds / average_seconds_in_four_years
+ years_old
+end
+
+puts customer_years_old(customer_birth_day, customer_birth_month, customer_birth_year)
