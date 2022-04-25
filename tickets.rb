87e3368f03d39667d3ab1a013e26641c18c87611
André L Josefino <ALJosefino@gmail.com>
2022-04-25 00:20:04 -0300

    conditional accord to age, kid, teeanger, adult, elderly and if is a student or no

--- tickets.rb
+++ tickets.rb
@@ -1,40 +1,28 @@
 # https://github.com/centeno/FirstApisulCodingDojo/blob/master/problem.txt
-
 # These are price of tickets to:
 # elderly: US$ 6.00
 # kids: US$ 5.50
 # students: US$ 8.00
-
 # Every day in a week it's price off, each day change for specific target public
 # On Monday:
 # elderly, kids, students: 10% off
-
 # On Tuesday:
 # elderly and kids: 15% off
 # students: 5% off 
-
 # On Wednesday:
 # elderly: 40% off
 # kids: 30% off
 # students: 50% off
-
 # On Thursday:
 # elderly and kids: 30% off
-
 # On Friday
 # kids: 11% off
-
 # On Saturday, Sunday and Holliday:
 # elderly: 5% off
-
 #This briefing was copied unashamed manner from @dwildt
-
 puts ("Hello! Here you may know how much customer will pay for a ticket")
-
 today = Time.new
-
 weekday = today.wday
-
 case weekday
 when 0
     #puts ("Today is sunday")
@@ -58,19 +46,17 @@ when 6
     #puts ("Today is saturday")
     today = "Saturday"
 end
-
 puts ("Today is  #{today}")
-
 puts ("Do you want manipulating #{today} to another weekday? yes or no") 
 change_today = gets.chomp.to_s
-
-while (change_today != "yes") and (change_today != "no") do
+while 
+    (change_today != "yes") and (change_today != "no") do
     puts ("Do you want manipulating #{today} to another weekday?") 
     puts ("Please type yes or no") 
     change_today = gets.chomp.to_s
 end
-
-if change_today == "yes"
+if 
+    change_today == "yes"
     puts ("Yes was typed, so it's allowed change weekday now.")
     puts ("According to this:")
     puts ("Type number: O = today is sunday")
@@ -81,7 +67,6 @@ if change_today == "yes"
     puts ("Type number: 5 = today is friday")
     puts ("Type number: 6 = today is saturday")
     today = gets.chomp.to_i
-    
         case today
         when 0
             today = "Sunday" 
@@ -105,11 +90,13 @@ if change_today == "yes"
             today = "Saturday"
             puts ("So, now you choose that today is saturday")
         else 
-                while (today != 0) and (today != 1) and 
-                (today != 2) and (today != 3) and 
-                (today != 4) and (today != 5) and
-                (today != 6)  do
-                    
+                while   (today != 0) and 
+                        (today != 1) and 
+                        (today != 2) and 
+                        (today != 3) and 
+                        (today != 4) and 
+                        (today != 5) and
+                        (today != 6)  do
                 puts ("According to this:")
                 puts ("Type number: O = today is sunday")
                 puts ("Type number: 1 = today is monday")
@@ -144,81 +131,79 @@ if change_today == "yes"
                         today = "Saturday"
                         puts ("So, now you choose that today is saturday")
                     end
-
-elsif change_today == "no"
-puts ("Weekday keeps on #{today}")
+elsif 
+    change_today == "no"
+    puts ("Weekday keeps on #{today}")
 end
-
 puts ("What is customer birth year? Example: 1999")
 customer_birth_year = gets.chomp.to_i
-
 puts ("What is customer birth month? Example: 04 = April")
 customer_birth_month = gets.chomp.to_i
-
 puts ("What is customer birth day? Example: 10")
 customer_birth_day = gets.chomp.to_i
-
 puts ("Do you want change this customer birthdate #{customer_birth_day}/#{customer_birth_month}/#{customer_birth_year}? yes or no?")
 change_customer_birthdate = gets.chomp.to_s
-
-while (change_customer_birthdate == "yes") do
-
+while 
+    (change_customer_birthdate == "yes") do
     puts ("What is customer birth year? Example: 1999")
     customer_birth_year = gets.chomp.to_i
-
     puts ("What is customer birth month? Example: 04 = April")
     customer_birth_month = gets.chomp.to_i
-
     puts ("What is customer birth day? Example: 10")
     customer_birth_day = gets.chomp.to_i
-
     puts ("Do you want change this customer birthdate #{customer_birth_day}/#{customer_birth_month}/#{customer_birth_year}? yes or no?")
-    change_customer_birthdate = gets.chomp.to_s
-    
+    change_customer_birthdate = gets.chomp.to_s 
 end
-
 require 'date'
-
-def age_in_years(day, month, year)   
+def 
+ age_in_years(day, month, year)   
  birthdate = Time.new(year, month, day)
  average_seconds_in_four_years = 31557600
  seconds = (Time.now - birthdate).to_i
  years_old = seconds / average_seconds_in_four_years
  years_old
 end
-
 customer_years_old = age_in_years(customer_birth_day, customer_birth_month, customer_birth_year)
-
 puts ("Customer is #{customer_years_old} years old")
-
 puts ("What's customer name?")
 customer_name = gets.chomp.to_s
-
 puts ("Customer #{customer_name} was born #{customer_birth_day}/#{customer_birth_month}/#{customer_birth_year} todays is #{customer_years_old} years old")
-
-if customer_years_old <= 12
+if 
+    customer_years_old <= 12
     puts ("This is kid customer")
     customer_kid = true
     customer_elderly = false
     customer_teenager_adult = false
-
-elsif customer_years_old >= 55
+elsif 
+    customer_years_old >= 55
     puts ("This is elderly customer")
     customer_elderly = true
     customer_kid = false
     customer_teenager_adult = false
-
 elsif
     puts ("teenager, young adult or adult")
     customer_teenager_adult = true
     customer_kid = false
     customer_elderly = false
 end
-
-if customer_years_old < 7 
+if 
+    customer_years_old < 7 
     puts ("This customer is too young to be a student.")
-
-elsif customer_years_old > 7
+elsif 
+    customer_years_old > 7
     puts ("Is this customer a student? Answer example: yes or no.")
-    customer_student = gets.chomp.to_s
+    customer_student = gets.chomp.to_s 
+    while 
+        (customer_student != "yes") and (customer_student != "no")
+        puts ("Please type yes or no, to confirm if a student")
+        customer_student = gets.chomp.to_s
+    end
+        if 
+            customer_student == "yes"
+            customer_student = true
+        elsif 
+            customer_student == "no"
+            customer_student = false
+        end
 end
+puts customer_student
